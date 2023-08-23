58b49192e529bf7cdc988aa9bfe817fb6a8ec97d
dlevak92 <dino-9@gmail.com>
2023-08-23 20:52:51 +0200

    Dodan vue3-burger-menu + njegov css

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="dashboard-view">
     <div class="dashboard">
+      <Slide noOverlay>
+      <a id="home" href="#">
+        <span>Home</span>
+      </a>
+    </Slide>
       <img alt="GuaranteeBox Logo" src="../assets/GuaranteeBox_Logo.png">
     </div>
     <div>
@@ -10,12 +15,20 @@
 </template>
 
 <script>
+import { Slide } from 'vue3-burger-menu';
+
+
 export default {
   name: 'DashboardView',
+  components: {
+    Slide,
+  },
 };
 </script>
 
 <style scoped>
+
+
 .dashboard-view {
   background-color: #471AA1;
   min-height: 100vh;
